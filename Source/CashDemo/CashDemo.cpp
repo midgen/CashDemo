523e54b9fc8f35d28f50fbfdd8eb50b0297e0c06
Chris Ashworth <chrisashworth@appzeit.com>
2017-11-01 15:30:31 +0100

    Initial bare project commit

---
+++ Source/CashDemo/CashDemo.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CashDemo.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CashDemo, "CashDemo" );
